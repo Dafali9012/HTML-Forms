ebec78b414ab55a5756746d69fe7cdfe577512cf
Daniel <Daniel.falklimberg@gmail.com>
2019-10-22 14:14:18 +0200

    preventDefault()

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let presentations = new Array()
-
+/*
 function onSubmit() {
     $("#form-information").hide()
     presentation = ""
     return false
 }
+*/
+$("#send-button").on("submit", function(e){
+    e.preventDefault()
+    $("#form-information").hide()
+})
